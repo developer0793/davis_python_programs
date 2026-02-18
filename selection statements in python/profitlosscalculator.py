ae1f213a6349f3e4fce6fe621faa01148a04cfa3
developer0793 <aurixisintellix@gmail.com>
2026-02-19 02:19:34 +0530

    Program to calculate profit or loss

--- selection statements in python/profitlosscalculator.py
+++ selection statements in python/profitlosscalculator.py
@@ -5,18 +5,16 @@ cp = float(input("Enter cost price(in Rs): "))
 if(cp <= 0):
     print("Invalid cost price")
     exit() #exiting the program if cost price is invalid
+#input of selling price
+sp = float(input("Enter selling price(in Rs): "))   
+#to validate selling price
+if(sp <= 0):        
+    print("Invalid selling price")
+    exit() #exiting the program if selling price is invalid
+#calculating profit or loss
+if(sp > cp):
+    print("Profit: Rs", (sp-cp))
+elif(cp > sp):
+    print("Loss: Rs", (cp-sp))
 else:
-    #input of selling price
-    sp = float(input("Enter selling price(in Rs): "))   
-    #to validate selling price
-    if(sp <= 0):        
-        print("Invalid selling price")
-        exit() #exiting the program if selling price is invalid
-    else:
-        #calculating profit or loss
-        if(sp > cp):
-            print("Profit: Rs", (sp-cp))
-        elif(cp > sp):
-            print("Loss: Rs", (cp-sp))
-        else:
-            print("No profit no loss")
+    print("No profit no loss")
