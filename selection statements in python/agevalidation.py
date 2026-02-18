1cb8b65b8be3e67b3b2a70105e081d77ec6f63b7
developer0793 <aurixisintellix@gmail.com>
2026-02-18 16:08:47 +0530

    Selection statements in python second session of 18th feb

---
+++ selection statements in python/agevalidation.py
@@ -0,0 +1,9 @@
+#input of age from user
+age = int(input("Enter your age(in year): "))
+#age validation
+if(age > 0):
+    print("Valid age")
+else:
+    print("Invalid age")
+
+print("-----------------------------------")
